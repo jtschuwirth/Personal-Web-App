66c8f406f26bed95d6537b671abe26691708e00f
jtschuwirth <jtschuwirth@uc.cl>
2022-10-12 18:11:03 -0300

    version 1.0 working

---
+++ components/Game/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./style.module.css"
+import { useState } from "react";
+import axios from "axios"
+
+export const Game  = () => {
+  const [phrase, setPhrase] = useState("_______________________");
+  async function handleClick() {
+    let response = await axios.get("https://59fxcxkow4.execute-api.us-east-1.amazonaws.com/dev/icebreakers/phrases")
+    setPhrase(response.data[0].phrase)
+    console.log(response)
+
+  }
+  return (
+    <div className={styles.gameContainer}>
+      <span>{phrase}</span>
+      <button className={styles.btn} onClick={() => handleClick()}>Get new prompt</button>
+    </div>
+
+  );
+};
